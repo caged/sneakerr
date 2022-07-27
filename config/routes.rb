cf97c28e7b7f7f8da4601dea4226035d64d8551a
Justin Palmer <encytemedia@gmail.com>
2022-07-27 09:51:02 -0500

    add site feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  post 'site/add', as: :add
+  post 'site/remove', as: :remove
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "site#index"
 end
