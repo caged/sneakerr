af314e2f3cdd9cfa4770f3056916690bd7f15d6a
Justin Palmer <encytemedia@gmail.com>
2022-07-28 11:09:24 -0500

    add sneaker scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :sneakers
   post 'site/add', as: :add
   post 'site/remove', as: :remove
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
