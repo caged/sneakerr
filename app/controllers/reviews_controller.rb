6093c5cd1d5e0ca6a8852d6eb285a73f051f2433
Justin Palmer <encytemedia@gmail.com>
2022-08-12 08:52:02 -0500

    turbo reviews take 2

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -22,17 +22,10 @@ class ReviewsController < AuthedController
 
   # POST /reviews or /reviews.json
   def create
-    @review = Review.new(review_params)
+    @review = @sneaker.reviews.new(review_params)
+    @review.user = current_user
 
-    respond_to do |format|
-      if @review.save
-        format.html { redirect_to review_url(@review), notice: "Review was successfully created." }
-        format.json { render :show, status: :created, location: @review }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
-    end
+    @review.save
   end
 
   # PATCH/PUT /reviews/1 or /reviews/1.json
